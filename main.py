4ea4d8a43910a150229064f1ab471d476ae7a4ea
Antonio <antoniodainok@gmail.com>
2021-01-05 08:59:09 +0100

    final commit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 from turtle import Turtle, Screen
-import pandas
-
-SCORE = 0
+import pandas as pd
 
 
 # # Screen setup
@@ -11,26 +9,42 @@ image = "blank_states_img.gif"
 screen.bgpic(image)
 
 
-# # # Getting the states data
-states_data = pandas.read_csv("50_states.csv")
+# Getting the states data
+states_data = pd.read_csv("50_states.csv")
+all_states = states_data.state.to_list()
 
 
-is_game_on = True
-while is_game_on:
-    user_answer = screen.textinput(title=f"States correct {SCORE}/{len(states_data.state)}", prompt="Guess a state: ").lower()
+right_guesses = []
+states_to_learn = []
+
+
+
+while len(right_guesses) < 50:
+    user_answer = screen.textinput(title=f"States correct {len(right_guesses)}/{len(states_data.state)}", prompt="Guess a state: ").title()
+
+    if user_answer == "Exit":
+        for item in right_guesses:
+            if item in all_states:
+                all_states.remove(item)
+        states_to_learn.append(all_states)
+        df = pd.DataFrame(states_to_learn)
+        df.to_csv("states_to_learn.csv")
+        break
+
     for state in states_data.state:
-        if user_answer == state.lower():
-            # increase score, get state coordinates, goto coordinates
-            SCORE += 1
+        if user_answer == state:
+            if state not in right_guesses:
+                right_guesses.append(user_answer)
             x_cor = states_data.x[states_data.state == state].max()
             y_cor = states_data.y[states_data.state == state].max()
             state_name = Turtle()
             state_name.penup()
             state_name.goto(x_cor, y_cor)
+            state_name.hideturtle()
             state_name.write(state, align="center", font=("arial", 10, "normal"))
+        
             
 
 
 
-screen.exitonclick()
 
